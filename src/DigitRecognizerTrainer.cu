769c28020c93cab71f0c352b31b6cdee1768f552
Pranav Marathe <pmarathe25@gmail.com>
2017-08-14 16:31:46 -0400

    Adds function to enforce uniqueness of output

--- src/DigitRecognizerTrainer.cu
+++ src/DigitRecognizerTrainer.cu
@@ -6,6 +6,18 @@
 #include "NeuralNetworkOptimizer.hpp"
 #include <vector>
 #include <fstream>
+
+template <typename Matrix>
+inline Matrix mseUnique(const Matrix& networkOutput, const Matrix& expectedOutput) {
+    // Ensure that only one of the outputs goes high.
+    return ((expectedOutput - networkOutput).pow(2) / 2).addVector((networkOutput.weightedSum(0) - 1) / 10);
+}
+
+template <typename Matrix>
+inline Matrix mseUnique_prime(const Matrix& networkOutput, const Matrix& expectedOutput) {
+    return networkOutput - expectedOutput.addVector((networkOutput.weightedSum(0) - 1) / 10);
+}
+
 // Define layers using a custom matrix class.
 typedef StealthAI::SigmoidFCL<StealthMatrix_F> SigmoidFCL_F;
 typedef StealthAI::LeakyReLUFCL<StealthMatrix_F> LeakyReLUFCL_F;
@@ -15,7 +27,7 @@ using NeuralNetwork_F = StealthAI::NeuralNetwork<StealthMatrix_F, Layers...>;
 // Define a dataset using a custom matrix class;
 typedef StealthAI::DataSet<StealthMatrix_F> DataSet_F;
 // Define an optimizer using a custom matrix class.
-typedef StealthAI::NeuralNetworkOptimizer<StealthMatrix_F, StealthAI::mse<StealthMatrix_F>, StealthAI::mse_prime<StealthMatrix_F>> NeuralNetworkOptimizer_MSE_F;
+typedef StealthAI::NeuralNetworkOptimizer<StealthMatrix_F, mseUnique<StealthMatrix_F>, mseUnique_prime<StealthMatrix_F>> NeuralNetworkOptimizer_MSEUNIQUE_F;
 
 int main() {
     // Create some layers.
@@ -24,7 +36,7 @@ int main() {
     // Create the network.
     NeuralNetwork_F<SigmoidFCL_F, SigmoidFCL_F> digitRecognizer(inputLayer, outputLayer);
     // Create an optimizer.
-    NeuralNetworkOptimizer_MSE_F optimizer;
+    NeuralNetworkOptimizer_MSEUNIQUE_F optimizer;
     // Load data.
     DataSet_F trainingInputs;
     DataSet_F trainingExpectedOutputs;
